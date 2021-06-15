7911b32cf482b7b9d9e1e0f0e846912b36db938b
Andy Freeland <andy@andyfreeland.net>
2021-06-14 23:27:30 -0700

    Add a formula for cargo-outdated
    https://github.com/kbknapp/cargo-outdated

---
+++ cargo-outdated.rb
@@ -0,0 +1,27 @@
+class CargoOutdated < Formula
+  desc "A cargo subcommand for displaying when Rust dependencies are out of date"
+  homepage "https://github.com/kbknapp/cargo-outdated"
+  url "https://github.com/kbknapp/cargo-outdated/archive/refs/tags/v0.9.15.tar.gz"
+  sha256 "8415befccdaa4328905c6e66cf3c95ebfc70ebda2fb82fd4f57391d37da60561"
+  license "MIT"
+  head "https://github.com/kbknapp/cargo-outdated"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cargo-outdated`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
